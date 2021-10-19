a9ff5ea2ca6ef714d22148eb534e4b66154644f1
Uwe Sauerbrei <info@uwe-sauerbrei.de>
2021-10-19 13:12:23 +0200

    Initial commit

---
+++ src/main/java/de/ostfale/sbdatajdbc/SbDataJdbcApplication.java
@@ -0,0 +1,13 @@
+package de.ostfale.sbdatajdbc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbDataJdbcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SbDataJdbcApplication.class, args);
+    }
+
+}
---
+++ src/test/java/de/ostfale/sbdatajdbc/SbDataJdbcApplicationTests.java
@@ -0,0 +1,13 @@
+package de.ostfale.sbdatajdbc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbDataJdbcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
