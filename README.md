e2b296b82a02fefd3ce38c44a86c09dcb6b3e7f8
Uwe Sauerbrei <info@uwe-sauerbrei.de>
2021-10-21 19:13:13 +0200

    Add web interface
    + provide api functions
    + add repository tests
    + add README

---
+++ README.md
@@ -0,0 +1,19 @@
+# Configuration
+
+## Timestamps
+
++ add annotation : `@EnableJdbcAuditing` to main class
+
+### created
+
+````java
+@CreatedDate 
+private Date createdDate;
+````
+
+### lastupdated
+
+````java
+@LastModifiedDate
+private Date lastModified;
+````
--- src/test/java/de/ostfale/sbdatajdbc/repository/MinionRepositoryTest.java
+++ src/test/java/de/ostfale/sbdatajdbc/repository/MinionRepositoryTest.java
@@ -1,14 +1,15 @@
 package de.ostfale.sbdatajdbc.repository;
 
 import de.ostfale.sbdatajdbc.model.Minion;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.data.jdbc.DataJdbcTest;
+import org.springframework.boot.test.context.SpringBootTest;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.*;
 
-@DataJdbcTest
+@DisplayName("Test Minion Repository services")
+@SpringBootTest
 class MinionRepositoryTest {
 
     @Autowired
@@ -20,11 +21,26 @@ class MinionRepositoryTest {
     }
 
     @Test
+    @DisplayName("Test creation of a Minion object")
+    void createMinionTest() {
+        // given
+        Minion minion = new Minion();
+        minion.setName("Bob");
+        // when
+        assertNull(minion.getId());
+        Minion createdMinion = minionRepository.save(minion);
+        // then
+        assertNotNull(createdMinion);
+        assertNotNull(createdMinion.getId());
+        assertEquals("Bob", createdMinion.getName());
+    }
+
+/*    @Test
     void TestMinionDefaultSavings() {
         Minion before = new Minion();
 
         assertNull(before.getId());
         Minion after = minionRepository.save(before);
         assertNotNull(after.getId());
-    }
+    }*/
 }
